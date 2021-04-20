87dac4801b2a538ffd81fe51814352e56d574ea1
Jonathan Buch <jbuch@synyx.de>
2021-04-20 11:14:07 +0200

    Use newer apt module
    * Rewrites deprecated parts

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -9,9 +9,11 @@ class influxdb::repo::apt {
     location    => "https://repos.influxdata.com/${_operatingsystem}",
     release     => $::lsbdistcodename,
     repos       => 'stable',
-    key         => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-    key_source  => 'https://repos.influxdata.com/influxdb.key',
-    include_src => false,
+    key         => {
+      'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+      'source' => 'https://repos.influxdata.com/influxdb.key',
+    },
+    include     => { 'src' => false },
   }
 
   Apt::Source['repos.influxdata.com'] -> Package<| tag == 'influxdb' |>
